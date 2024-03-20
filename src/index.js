6c09603d5b047733c4832081d54b60a47b4ca2e5
LauraDuane <redlunarcloud@gmail.com>
2024-03-20 00:16:34 -0400

    finished setting up the data
    updating all the current weather information for input city

--- src/index.js
+++ src/index.js
@@ -7,15 +7,37 @@ function updateWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   console.log(response.data);
 
-  timeElement.innerHTML = "tuesday 11:18";
+  timeElement.innerHTML = formatDate(date);
   windElement.innerHTML = `${response.data.wind.speed}mph`;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   descriptionElement.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
