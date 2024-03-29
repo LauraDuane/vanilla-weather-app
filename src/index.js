5d01ba5dac0cf4db8a6e38b13072dc40b22b012f
LauraDuane <redlunarcloud@gmail.com>
2024-03-28 21:07:00 -0400

    updated the rest of the api info for forcast
    fixed a couple mis-spellings and hick-ups

--- src/index.js
+++ src/index.js
@@ -57,6 +57,11 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
 function getforecast(city) {
   let apiKey = "e6o3f7bda6befc81749240et021aae9f";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
@@ -64,29 +69,29 @@ function getforecast(city) {
 }
 
 function displayForecast(response) {
-  let days = ["sun", "mon", "tues", "wed", "thur", "fri", "sat"];
-
   let forecastHtml = "";
 
-  response.data.daily.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="weather-forecast-day">
-<div class="weather-forecast-date">${day}</div>
-              <div >
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="weather-forecast-day">
+<div class="weather-forecast-date">${formatDay(day.time)}</div>
+              
               <img src="${
                 day.condition.icon_url
               }" class="weather-forecast-icon" />
-              </div>
+              
               <div class="weather-forecast-temperatures">
               <div class="weather-forecast-temperature">
-                <strong>${math.round(day.temperature.maximum)}°</strong>
+                <strong>${Math.round(day.temperature.maximum)}°</strong>
                 </div>
-                <div class="weather-forecast-temperature"> ${math.round(
+                <div class="weather-forecast-temperature"> ${Math.round(
                   day.temperature.minimum
                 )}°</div>
               </div>
                 </div>`;
+    }
   });
 
   let forcastElement = document.querySelector("#forecast");
